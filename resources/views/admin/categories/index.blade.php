c632465313e1cfb14537afaa907a4d91dec35f8c
unknown <sagor.askar@gmail.com>
2025-07-05 22:50:08 +0600

    Added a dropdown for category in the landing page

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -1,17 +1,17 @@
 @extends('layouts.admin')
 @section('content')
 <div class="content">
-        <div style="margin-bottom: 10px;" class="row">
-            <div class="col-lg-12">
-                    <a class="btn btn-success" href="{{ route('admin.categories.create') }}">
-                        Add   {{ trans('cruds.category.title_singular') }}
-                    </a>
-            </div>
+    <div style="margin-bottom: 10px;" class="row">
+        <div class="col-lg-12">
+            <a class="btn btn-success" href="{{ route('admin.categories.create') }}">
+                Add {{ trans('cruds.category.title_singular') }}
+            </a>
         </div>
+    </div>
     @if(session('success'))
-        <div class="alert alert-success" role="alert">
-            {{ session('success') }}
-        </div>
+    <div class="alert alert-success" role="alert">
+        {{ session('success') }}
+    </div>
     @endif
     <div class="row">
         <div class="col-lg-12">
@@ -31,10 +31,10 @@
                                         Name
                                     </th>
                                     <th>
-                                       Description
+                                        Logo
                                     </th>
                                     <th>
-                                        Logo
+                                        Description
                                     </th>
                                     <th>
                                         {{ trans('cruds.category.fields.status') }}
@@ -46,47 +46,52 @@
                             </thead>
                             <tbody>
                                 @foreach($categories as $key => $category)
-                                    <tr data-entry-id="{{ $category->id }}">
-                                        <td>
+                                <tr data-entry-id="{{ $category->id }}">
+                                    <td>
 
-                                        </td>
-                                        <td>
-                                            {{ $category->name ?? '' }}
-                                        </td>
-                                        <td><img src="{{ asset($category->logo) }}" alt="Image" width="40"></td>
-                                        <td>
-                                            {{ $category->description ?? '' }}
-                                        </td>
-                                        @if($category->status == 1)
-                                        <td>
-                                            <span class="badge badge-success" style="background-color: green">Active</span>
-                                        </td>
-                                        @else
-                                            <td>
-                                                <span class="badge badge-danger">Inactive</span>
-                                            </td>
-                                        @endif
+                                    </td>
+                                    <td>
+                                        {{ $category->name ?? '' }}
+                                    </td>
+                                    @if($category->logo)
+                                    <td><img src="{{ asset($category->logo) }}" alt="Image" width="40"></td>
+                                    @else
+                                    <td><span class="badge badge-danger">No Logo Attached!</span></td>
 
-                                        <td>
-                                                <a class="btn btn-xs btn-info" href="{{ route('admin.categories.edit', $category->id) }}">
-                                                    {{ trans('global.edit') }}
-                                                </a>
+                                    @endif
+                                    <td>
+                                        {{ $category->description ?? '' }}
+                                    </td>
+                                    @if($category->status == 1)
+                                    <td>
+                                        <span class="badge badge-success" style="background-color: green">Active</span>
+                                    </td>
+                                    @else
+                                    <td>
+                                        <span class="badge badge-danger">Inactive</span>
+                                    </td>
+                                    @endif
 
-                                                <form action="{{ route('admin.categories.destroy', $category->id) }}" method="POST" onsubmit="return confirm('{{ trans('global.areYouSure') }}');" style="display: inline-block;">
-                                                    <input type="hidden" name="_method" value="DELETE">
-                                                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                                                    <input type="submit" class="btn btn-xs btn-danger" value="{{ trans('global.delete') }}">
-                                                </form>
+                                    <td>
+                                        <a class="btn btn-xs btn-info" href="{{ route('admin.categories.edit', $category->id) }}">
+                                            {{ trans('global.edit') }}
+                                        </a>
 
-                                        </td>
+                                        <form action="{{ route('admin.categories.destroy', $category->id) }}" method="POST" onsubmit="return confirm('{{ trans('global.areYouSure') }}');" style="display: inline-block;">
+                                            <input type="hidden" name="_method" value="DELETE">
+                                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                                            <input type="submit" class="btn btn-xs btn-danger" value="{{ trans('global.delete') }}">
+                                        </form>
 
-                                    </tr>
+                                    </td>
+
+                                </tr>
                                 @endforeach
                             </tbody>
                         </table>
                         @if ($categories->hasPages())
 
-                            {{ $categories->links() }}
+                        {{ $categories->links() }}
                         @endif
                     </div>
                 </div>
@@ -101,50 +106,67 @@
 @section('scripts')
 @parent
 <script>
-    $(function () {
-  let dtButtons = $.extend(true, [], $.fn.dataTable.defaults.buttons)
-@can('category_delete')
-  let deleteButtonTrans = '{{ trans('global.datatables.delete') }}'
-  let deleteButton = {
-    text: deleteButtonTrans,
-    url: "{{ route('admin.categories.massDestroy') }}",
-    className: 'btn-danger',
-    action: function (e, dt, node, config) {
-      var ids = $.map(dt.rows({ selected: true }).nodes(), function (entry) {
-          return $(entry).data('entry-id')
-      });
+    $(function() {
+        let dtButtons = $.extend(true, [], $.fn.dataTable.defaults.buttons)
+        @can('category_delete')
+        let deleteButtonTrans = '{{ trans('
+        global.datatables.delete ') }}'
+        let deleteButton = {
+            text: deleteButtonTrans
+            , url: "{{ route('admin.categories.massDestroy') }}"
+            , className: 'btn-danger'
+            , action: function(e, dt, node, config) {
+                var ids = $.map(dt.rows({
+                    selected: true
+                }).nodes(), function(entry) {
+                    return $(entry).data('entry-id')
+                });
 
-      if (ids.length === 0) {
-        alert('{{ trans('global.datatables.zero_selected') }}')
+                if (ids.length === 0) {
+                    alert('{{ trans('
+                        global.datatables.zero_selected ') }}')
 
-        return
-      }
+                    return
+                }
 
-      if (confirm('{{ trans('global.areYouSure') }}')) {
-        $.ajax({
-          headers: {'x-csrf-token': _token},
-          method: 'POST',
-          url: config.url,
-          data: { ids: ids, _method: 'DELETE' }})
-          .done(function () { location.reload() })
-      }
-    }
-  }
-  dtButtons.push(deleteButton)
-@endcan
+                if (confirm('{{ trans('
+                        global.areYouSure ') }}')) {
+                    $.ajax({
+                            headers: {
+                                'x-csrf-token': _token
+                            }
+                            , method: 'POST'
+                            , url: config.url
+                            , data: {
+                                ids: ids
+                                , _method: 'DELETE'
+                            }
+                        })
+                        .done(function() {
+                            location.reload()
+                        })
+                }
+            }
+        }
+        dtButtons.push(deleteButton)
+        @endcan
 
-  $.extend(true, $.fn.dataTable.defaults, {
-    orderCellsTop: true,
-    order: [[ 1, 'desc' ]],
-    pageLength: 100,
-  });
-  let table = $('.datatable-SubCompany:not(.ajaxTable)').DataTable({ buttons: dtButtons })
-  $('a[data-toggle="tab"]').on('shown.bs.tab click', function(e){
-      $($.fn.dataTable.tables(true)).DataTable()
-          .columns.adjust();
-  });
+        $.extend(true, $.fn.dataTable.defaults, {
+            orderCellsTop: true
+            , order: [
+                [1, 'desc']
+            ]
+            , pageLength: 100
+        , });
+        let table = $('.datatable-SubCompany:not(.ajaxTable)').DataTable({
+            buttons: dtButtons
+        })
+        $('a[data-toggle="tab"]').on('shown.bs.tab click', function(e) {
+            $($.fn.dataTable.tables(true)).DataTable()
+                .columns.adjust();
+        });
 
-})
+    })
 
 </script>
 
--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -23,12 +23,12 @@
 
                 @if (Auth::check())
 
-                  <li><a href="{{ route('customize') }}">Customize Jobs</a></li>
+                <li><a href="{{ route('customize') }}">Customize Jobs</a></li>
 
-                    @if(Auth::user()->role_id == 2)
-                      <li><a href="{{ route('uploads') }}">Upload</a></li>
-                    @endif
-              @endif
+                @if(Auth::user()->role_id == 2)
+                <li><a href="{{ route('uploads') }}">Upload</a></li>
+                @endif
+                @endif
             </ul>
             <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
         </nav>
@@ -36,45 +36,35 @@
 
         <!-- User Profile or Login / Registration -->
         @if (Auth::check())
-            <!-- User Profile -->
-            <div style="position: relative;">
-                <a href="#" onclick="toggleDropdown()"
-                   style="display: flex; align-items: center; text-decoration: none; color: black;">
-                    <img src="{{ asset(Auth::user()->image ? Auth::user()->image : 'assets/img/user/default-user.png') }}"
-                         alt="Profile"
-                         style="width: 35px; height: 35px; border-radius: 50%; object-fit: cover; margin-right: 10px;">
-                    <span>{{ Auth::user()->name }}</span>
+        <!-- User Profile -->
+        <div style="position: relative;">
+            <a href="#" onclick="toggleDropdown()" style="display: flex; align-items: center; text-decoration: none; color: black; margin-left: 0.8rem;">
+                <img src="{{ asset(Auth::user()->image ? Auth::user()->image : 'assets/img/user/default-user.png') }}" alt="Profile" style="width: 35px; height: 35px; border-radius: 50%; object-fit: cover; margin-right: 10px;">
+                <span>{{ Auth::user()->name }}</span>
+            </a>
+
+            <!-- Dropdown Menu -->
+            <div id="dropdownMenu" style="display: none; position: absolute; top: 40px; left: 0; background-color: white; border: 1px solid #ccc; box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);">
+                <a href="#" style="display: block; padding: 10px; text-decoration: none; color: black; width: 8rem;">
+                    <i class="fa fa-list"></i> {{ trans('global.dashboard') }}
+
                 </a>
-
-                <!-- Dropdown Menu -->
-                <div id="dropdownMenu"
-                     style="display: none; position: absolute; top: 40px; left: 0; background-color: white; border: 1px solid #ccc; box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);">
-                    <a href="#"
-                       style="display: block; padding: 10px; text-decoration: none; color: black; width: 8rem;">
-                        <i class="fa fa-list"></i> {{ trans('global.dashboard') }}
-
-                    </a>
-                    <form action="{{ route('logout') }}" method="POST" style="margin: 0;">
-                        @csrf
-                        <button type="submit"
-                                style="display: block; width: 100%; padding: 10px; background: none; border: none; text-align: left; cursor: pointer; color: black;">
-                            <i class="fa fa-sign-out"></i> {{ trans('global.logout') }}
-                        </button>
-                    </form>
-                </div>
+                <form action="{{ route('logout') }}" method="POST" style="margin: 0;">
+                    @csrf
+                    <button type="submit" style="display: block; width: 100%; padding: 10px; background: none; border: none; text-align: left; cursor: pointer; color: black;">
+                        <i class="fa fa-sign-out"></i> {{ trans('global.logout') }}
+                    </button>
+                </form>
             </div>
+        </div>
         @else
-            <!-- Login Button -->
-            <div>
-                <a class="btn-getstarted flex-md-shrink-0" href="{{ route('signin') }}">SIGN IN</a>
-            </div>
+        <!-- Login Button -->
+        <div>
+            <a class="btn-getstarted flex-md-shrink-0" href="{{ route('signin') }}">SIGN IN</a>
+        </div>
 
         @endif
-
-
-
     </div>
-
 
     <script>
         function toggleDropdown() {
@@ -87,5 +77,6 @@
                 dropdown.style.display = 'none';
             }
         });
+
     </script>
 </header>
