e4ff69af22c81f872df0e72b79ed6e2dd4ecdb9b
unknown <sagor.askar@gmail.com>
2025-11-07 00:58:36 +0600

    Image view all page designed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,33 +3,7 @@
 <main class="main">
 
     <!-- Hero Section -->
-    <section id="hero" class="hero section">
-        <div class="container">
-            <!-- search box with dropdown -->
-            <div class="custom-input-group" style="display: flex; align-items: center; gap: 10px;">
-
-                <!-- Dropdown -->
-                <select class="custom-form-control" style="max-width: 10rem;">
-                    <option value="all">All</option>
-                    <option value="photos">Photos</option>
-                    <option value="illustrations">Illustrations</option>
-                    <option value="vectors">Vectors</option>
-                </select>
-
-                <!-- Search Input -->
-                <input type="text" class="custom-form-control" placeholder="SEARCH IMAGE HERE">
-
-                <!-- Search Button -->
-                <div class="custom-input-group-append">
-                    <button class="custom-btn" type="button">
-                        <i class="fa fa-search"></i>
-                    </button>
-                </div>
-            </div>
-        </div>
-    </section>
-
-
+    @include('includes.hero')
 
     <!-- Best Collection -->
     <section class="section">
